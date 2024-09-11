6b00c9a74d3b96d06001ce5c318a58e622a756b1
Vitor Reis <reis_vitor@outlook.com>
2024-09-10 22:36:24 -0300

    feat: add dijkstra algorithm. refactor: adapt Router model to dijkstra

--- model/Router.java
+++ model/Router.java
@@ -1,13 +1,15 @@
 package model;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 
 public class Router {
    private String ip;
    private ArrayList<Connection> connections;
+   private Boolean permanent = false;
+   private Router from;
+   private Integer cost;
 
-   public Router(String ip) {
+  public Router(String ip) {
      this.ip = ip;
      this.connections = new ArrayList<Connection>();
    }
@@ -26,6 +28,10 @@ public class Router {
     this.connections = connections;
   }
 
+  public void addConnection(Connection connection) {
+    this.connections.add(connection);
+  }
+
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
@@ -39,8 +45,36 @@ public class Router {
     return this.ip.equals(router.getIp());
   }
 
+  public Boolean getPermanent() {
+    return permanent;
+  }
+
+  public void setPermanent(Boolean permanent) {
+    this.permanent = permanent;
+  }
+
+  public Router getFrom() {
+    return from;
+  }
+
+  public void setFrom(Router from) {
+    this.from = from;
+  }
+
+  public Integer getCost() {
+    return cost;
+  }
+
+  public void setCost(Integer cost) {
+    this.cost = cost;
+  }
+
   @Override
   public String toString() {
     return "Router{ip='" + this.ip + "'" + " connections=" + this.connections.toString() + "}";
+  }
+
+  public String toStringDijkstra() {
+    return "Router{ip=" + this.ip + " cost=" + this.getCost() + " from=" + this.getFrom() + "}";
   }
 }
